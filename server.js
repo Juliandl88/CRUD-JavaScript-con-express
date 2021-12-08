1253f7732df51c71147e7f6b108d57c6bca467f5
Julián De Lisio <76756541+Juliandl88@users.noreply.github.com>
2021-12-07 23:55:19 -0300

    first commit

---
+++ server.js
@@ -0,0 +1,20 @@
+const express = require("express");
+
+const app = express();
+
+const port = 3001;
+
+app.get("/", (req, res) => {
+
+    res.send({
+        data: "Hello World"
+    });
+
+});
+
+app.listen(port, () => {
+    
+        console.log(`Server running on port ${port}`);
+    
+}); 
+
