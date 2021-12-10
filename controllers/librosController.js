43c11b18538f539d003b969794f5047f66efa7f9
Julián De Lisio <76756541+Juliandl88@users.noreply.github.com>
2021-12-10 07:39:09 -0300

    Estructurar Borrar y Guardar

--- controllers/librosController.js
+++ controllers/librosController.js
@@ -1,5 +1,6 @@
 const conexion = require('../config/conexion');
 const libro = require('../model/libro'); 
+const borrar = require("fs");
 
 module.exports = {
 
@@ -23,9 +24,26 @@ module.exports = {
     },
 
     guardar:function(req, res){
+        console.log(req.body);
+        console.log(req.file.filename);
 
-       res.send(req.body);      
+        libro.insertar(conexion, req.body, req.file, function (err){   
+            res.redirect('/libros');
+        });
+           
 
+    },
+
+    eliminar:function(req, res){
+        console.log("Recepción de datos para eliminar");
+        console.log(req.params.id);
+        libro.retornarDatosID(conexion, req.params.id, function(err, registros){
+
+            var nombreImagen = "public/images/" + (registro[0].imagen);
+            res.send(nombreImagen);
+
+ 
+        })
     }
 
 }
--- routes/libros.js
+++ routes/libros.js
@@ -2,9 +2,31 @@ var express = require('express');
 var router = express.Router();
 const librosController = require('../controllers/librosController');
 
+const multer = require('multer');
+const fecha = Date.now();
+
+var rutaAlmacen = multer.diskStorage({
+
+    destination: function (req, file, cb) {
+        cb(null, 'public/images');
+    },
+    filename: function (req, file, cb) {
+        console.log(file)
+        cb(null, fecha + " " + file.originalname);
+    }
+
+});
+
+var cargar = new multer({
+
+    storage: rutaAlmacen    
+
+});
+
 /* GET home page. */
 router.get('/', librosController.index);      // GET /libros
 router.get('/crear', librosController.crear);   // GET /libros/crear
-router.post('/', librosController.guardar);   // POST /libros/guardar
+router.post('/', cargar.single("archivo"), librosController.guardar);   // POST /libros/guardar
+router.post("/eliminar/:id", librosController.eliminar);
 
 module.exports = router;    //exportar el router
